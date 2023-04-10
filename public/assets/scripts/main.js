bab95c43fe25e5b1fd0b063011ac19e762c8958f
Rafael <rafael.urquiaga@gmail.com>
2023-04-10 18:06:52 -0500

    feat(script): add main.js file

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,17 @@
+const start=()=>{
+
+    const menu = document.querySelector(".menu")
+    const menu__links=document.querySelector(".menuLinks")
+
+    //SELECTING menu items
+    const links__menu = document.querySelectorAll(".menuItem")
+
+    links__menu.forEach(link=> link.addEventListener("click", e=>{
+        menu__links.classList.toggle("hamburgerActive")
+    }))
+    menu.addEventListener("click", e=>{
+        menu__links.classList.toggle("hamburgerActive")
+    })
+}
+
+window.onload=()=>start();
