7cfd0ceef3a73266faa8bd2d5dcfb624fbb69557
William Riega <williammar954@hotmail.com>
2023-11-09 21:03:13 -0500

    Update everything

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,55 @@
-const start=()=>{
+const start = () => {
+    const menu = document.querySelector(".menu");
+    const menu__links = document.querySelector(".menuLinks");
 
-    const menu = document.querySelector(".menu")
-    const menu__links=document.querySelector(".menuLinks")
+    // SELECTING menu items
+    const links__menu = document.querySelectorAll(".menuItem");
 
-    //SELECTING menu items
-    const links__menu = document.querySelectorAll(".menuItem")
+    links__menu.forEach(link => link.addEventListener("click", (e) => {
+        menu__links.classList.toggle("hamburgerActive");
+    }));
 
-    links__menu.forEach(link=> link.addEventListener("click", e=>{
-        menu__links.classList.toggle("hamburgerActive")
-    }))
-    menu.addEventListener("click", e=>{
-        menu__links.classList.toggle("hamburgerActive")
-    })
-}
+    menu.addEventListener("click", (e) => {
+        menu__links.classList.toggle("hamburgerActive");
+    });
 
-window.onload=()=>start();
+    // Slider functionality
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+
+    let counter = 1;
+    const size = slides[0].clientWidth;
+
+    slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+    nextBtn.addEventListener('click', () => {
+        if (counter >= slides.length - 1) return;
+        slider.style.transition = 'transform 0.5s ease-in-out';
+        counter++;
+        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (counter <= 0) return;
+        slider.style.transition = 'transform 0.5s ease-in-out';
+        counter--;
+        slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    });
+
+    slider.addEventListener('transitionend', () => {
+        if (slides[counter].id === 'last-clone') {
+            slider.style.transition = 'none';
+            counter = slides.length - 2;
+            slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
+        if (slides[counter].id === 'first-clone') {
+            slider.style.transition = 'none';
+            counter = slides.length - counter;
+            slider.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        }
+    });
+};
+
+window.onload = () => start();
